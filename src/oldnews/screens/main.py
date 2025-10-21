2ffceba7f7802204f98374923e8fb3ae780b5761
Dave Pearson <davep@davep.org>
2025-10-21 11:27:12 +0100

    :sparkles: Add header and footer to the main screen

--- src/oldnews/screens/main.py
+++ src/oldnews/screens/main.py
@@ -1,12 +1,18 @@
 """Provides the main screen."""
 
 ##############################################################################
+# Textual imports.
+from textual.app import ComposeResult
+from textual.widgets import Footer, Header
+
+##############################################################################
 # Textual enhanced imports.
 from textual_enhanced.screen import EnhancedScreen
 
 ##############################################################################
 # Local imports.
 from .. import __version__
+
 
 ##############################################################################
 class Main(EnhancedScreen[None]):
@@ -16,5 +22,10 @@ class Main(EnhancedScreen[None]):
 
     COMMAND_MESSAGES = []
 
-### main.py ends here
+    def compose(self) -> ComposeResult:
+        """Compose the content of the main screen."""
+        yield Header()
+        yield Footer()
 
+
+### main.py ends here
