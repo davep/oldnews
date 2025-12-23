21bb1d3df3abd4a540480342d5c09263a30c43d4
Dave Pearson <davep@davep.org>
2025-12-23 16:22:52 +0000

    :sparkles: Add some indexes

--- src/oldnews/data/db.py
+++ src/oldnews/data/db.py
@@ -42,12 +42,68 @@ def initialise_database() -> TypeDAL:
     #
     # warning to stdout, otherwise.
     dal = TypeDAL(f"sqlite://{db_file()}", folder=data_dir(), config=TypeDALConfig())
+
+    # This need to try/except/pass seems massively unnecessary. Everything
+    # I'm reading seems to suggest that the migration system should handle
+    # this, not try and create the index if it exists, but it does and it
+    # gives an error. So, for now, this...
+
     dal.define(LocalArticleCategory)
+    try:
+        LocalArticleCategory.create_index(
+            "idx_local_article_category_article", LocalArticleCategory.article
+        )
+    except RuntimeError:
+        pass
+    try:
+        LocalArticleCategory.create_index(
+            "idx_local_article_category_category", LocalArticleCategory.category
+        )
+    except RuntimeError:
+        pass
+
     dal.define(LocalArticle)
+    try:
+        LocalArticle.create_index(
+            "idx_local_article_article_id", LocalArticle.article_id
+        )
+    except RuntimeError:
+        pass
+    try:
+        LocalArticle.create_index(
+            "idx_local_article_origin_stream_id", LocalArticle.origin_stream_id
+        )
+    except RuntimeError:
+        pass
+
     dal.define(LocalFolder)
+
     dal.define(LocalSubscription)
+    try:
+        LocalSubscription.create_index(
+            "idx_local_subscription_subscription_id", LocalSubscription.subscription_id
+        )
+    except RuntimeError:
+        pass
+
     dal.define(LocalSubscriptionCategory)
+    try:
+        LocalSubscriptionCategory.create_index(
+            "idx_local_subscription_category_subscription",
+            LocalSubscriptionCategory.subscription,
+        )
+    except RuntimeError:
+        pass
+    try:
+        LocalSubscriptionCategory.create_index(
+            "idx_local_subscription_category_category_id",
+            LocalSubscriptionCategory.category_id,
+        )
+    except RuntimeError:
+        pass
+
     dal.define(NavigationState)
+
     return dal
 
 
