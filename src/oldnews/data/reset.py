21be5e4587763415736b6d7c24fff672c42cd4cb
Dave Pearson <davep@davep.org>
2026-02-05 22:12:04 +0000

    :zap: Change the files removed in a cleanup

--- src/oldnews/data/reset.py
+++ src/oldnews/data/reset.py
@@ -17,7 +17,13 @@ def reset_data(logout: bool) -> None:
         logout: Should any token file be removed too?
     """
     to_remove: list[Path] = []
-    for pattern in ("*.table", "*.db", "*.log", *((".token",) if logout else ())):
+    for pattern in (
+        "*.db",
+        "*.db-shm",
+        "*.db-wal",
+        "*.log",
+        *((".token",) if logout else ()),
+    ):
         to_remove.extend(data_dir().glob(pattern))
     for data_file in to_remove:
         data_file.unlink()
